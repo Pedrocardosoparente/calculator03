1ad9484c2eba4eff6462cf191b736ffedc424fe0
Aluno <Aluno@10.85.198.36>
2024-06-04 14:05:03 -0300

    commit project calculator03

---
+++ src/Calculator3.java
@@ -0,0 +1,17 @@
+public class Calculator3 {
+    public double tax = .05;
+    public double tip = .15;
+    
+    public void findTotal(double price){
+        double total = price*(1+tax+tip);
+        System.out.println("$" +total);
+
+    }
+}
+class Calculator {
+
+    void findTotal(int i) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
